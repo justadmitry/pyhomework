e633b0adef35037943bdf75f41892573bd99f059
justadmitry <111776686+justadmitry@users.noreply.github.com>
2022-08-22 19:22:15 +0300

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,31 @@
+data1 = input("Введите дату: ")
+task1 = input("Введите задачу: ")
+run1 = data1 + task1
+print(run1)
+
+data2 = input("Введите дату: ")
+task2 = input("Введите задачу: ")
+data3 = input("Введите дату: ")
+task3 = input("Введите задачу: ")
+data4 = input("Введите дату: ")
+task4 = input("Введите задачу: ")
+run2 = data2 + task2
+run3 = data3 + task3
+run4 = data4 + task4
+print(run2)
+print(run3)
+print(run4)
+
+dictionary = {
+  
+}
+
+data5 = input("Введите дату: ")
+task5 = input("Введите задачу: ")
+data6 = input("Введите дату: ")
+task6 = input("Введите задачу: ")
+data7 = input("Введите дату: ")
+task7 = input("Введите задачу: ")
+dictionary[data5]: task5
+dictionary[data6]: task6
+dictionary[data7]: task7
